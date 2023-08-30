f08095afebe16850ab422bd7c87fe33b7c8de6ad
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-08-30 18:22:45 +0100

    making calls to openai working and 5 name suggestions are created

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,30 @@
+require 'openai'
+
 class ResultsController < ApplicationController
   def api_call
     input = params[:input]
-    redirect_to results_path
+    client = OpenAI::Client.new
+    prompt = "generate 5 names for a #{input}"
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: [{ "role": 'user', "content": prompt }],
+        temperature: 0.7
+      }
+    )
+    @response = response.dig('choices', 0, 'message', 'content')
   end
+    # response = client.ChatCompletetion.create(
+    #   parameters: {
+    #     model: 'gpt-3.5-turbo',
+    #     messages: [
+    #       {
+    #         "role": 'user',
+    #         "content": prompt
+    #       }
+    #     ],
+    #     temperature: 0.6
+    #   }
+    # )
+    # puts response.dig('choices', 0, 'message', 'content')
 end
