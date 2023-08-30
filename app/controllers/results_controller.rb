52b5bf9c1f073d9c22dfe7f8afdf48600bda83b3
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-08-30 21:41:43 +0100

    outputs are now clean

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -12,20 +12,19 @@ class ResultsController < ApplicationController
         temperature: 0.7
       }
     )
-    split = response.dig('choices', 0, 'message', 'content').split
-    @response = split.reject { |v| split.index(v).even? }
+    returned_result = response.dig('choices', 0, 'message', 'content').split(".")
+    @response = clean_result(returned_result)
   end
-    # response = client.ChatCompletetion.create(
-    #   parameters: {
-    #     model: 'gpt-3.5-turbo',
-    #     messages: [
-    #       {
-    #         "role": 'user',
-    #         "content": prompt
-    #       }
-    #     ],
-    #     temperature: 0.6
-    #   }
-    # )
-    # puts response.dig('choices', 0, 'message', 'content')
+
+  private
+
+  def clean_result(array)
+    clean_results = []
+    array.each do |result|
+      result.gsub!(/[\n\d]/, "")
+      clean_string = result
+      clean_results << clean_string
+    end
+    clean_results[1..-1]
+  end
 end
