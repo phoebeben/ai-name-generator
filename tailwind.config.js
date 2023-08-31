2ffa1481af216850e940542699802cbdd5ef1ec1
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-09-01 00:10:31 +0100

    styling for home page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
