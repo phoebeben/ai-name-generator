95d4ae21928d17cdb4f86b0c4ea26937bf5a6798
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-09-01 23:27:25 +0100

    added regenerate button to results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,18 +2,19 @@ require 'openai'
 
 class ResultsController < ApplicationController
   def api_call
-    input = params[:input]
-    client = OpenAI::Client.new
-    prompt = "generate 5 names for a #{input}"
-    response = client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo',
-        messages: [{ "role": 'user', "content": prompt }],
-        temperature: 0.7
-      }
-    )
-    returned_result = response.dig('choices', 0, 'message', 'content').split(".")
-    @response = clean_result(returned_result)
+    # input = params[:input]
+    # client = OpenAI::Client.new
+    # prompt = "generate 5 names for a #{input}"
+    # response = client.chat(
+    #   parameters: {
+    #     model: 'gpt-3.5-turbo',
+    #     messages: [{ "role": 'user', "content": prompt }],
+    #     temperature: 0.7
+    #   }
+    # )
+    # returned_result = response.dig('choices', 0, 'message', 'content').split(".")
+    # @response = clean_result(returned_result)4
+    @response = ["result 1", "result 2", "result 3", "result 4", "result 5"]
   end
 
   private
