fd3c18a225725ed16a6f7628f8a197718e544f07
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-09-02 22:15:29 +0100

    styled generate new name button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/', to: 'pages#home'
+  root 'pages#home'
   post '/results', to: 'results#api_call'
   get '/results', to: 'results#api_call'
   post '/results/regenerate_results', to: 'results#regenerate_results'
