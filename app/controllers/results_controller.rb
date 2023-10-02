9e200801365cc240f59e2231085174f924621fd7
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-10-02 19:36:51 +0100

    refactored results_controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,17 +4,8 @@ class ResultsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:regenerate_results]
   def api_call
     input = params[:input]
-    client = OpenAI::Client.new
     prompt = "generate 5 names for a #{input}"
-    response = client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo',
-        messages: [{ "role": 'user', "content": prompt }],
-        temperature: 0.7
-      }
-    )
-    returned_result = response.dig('choices', 0, 'message', 'content').split('.')
-    @response = clean_result(returned_result)
+    @response = make_api_request(prompt)
     @input = params[:input]
   end
 
@@ -25,28 +16,26 @@ class ResultsController < ApplicationController
     number_to_generate = 5 - results.length
     results_joined = results.join(" and ")
     prompt = "generate #{number_to_generate} names for a #{input}. I don't want the names #{results_joined} to be included."
-    client = OpenAI::Client.new
-    response = client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo',
-        messages: [{ "role": 'user', "content": prompt }],
-        temperature: 1
-      }
-    )
-    returned_result = response.dig('choices', 0, 'message', 'content').split('.')
-    response = clean_result(returned_result)
+    response = make_api_request(prompt)
     render json: response.to_json
   end
 
   private
 
+  def make_api_request(prompt)
+    client = OpenAI::Client.new
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: [{ "role": 'user', "content": prompt }],
+        temperature: 0.7
+      }
+    )
+    returned_result = response.dig('choices', 0, 'message', 'content').split('.')
+    clean_result(returned_result)
+  end
+
   def clean_result(array)
-    clean_results = []
-    array.each do |result|
-      result.gsub!(/[\n\d]/, '')
-      clean_string = result
-      clean_results << clean_string
-    end
-    clean_results[1..-1]
+    array.map { |result| result.gsub(/[\n\d]/, '') }[1..-1]
   end
 end
